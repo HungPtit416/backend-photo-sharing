dd44cfd2e7c51857e42f61e5bc14d80547ba0f68
Hung <hnguyentuan63@gmail.com>
2025-11-11 02:18:12 +0700

    feat: delete unnecessary files and icons

--- realtime/sseRouter.js
+++ realtime/sseRouter.js
@@ -5,20 +5,20 @@ const router = express.Router();
 const { addClient, removeClient, broadcast } = require("./sseManager");
 
 router.get("/stream", (req, res) => {
-  console.log("📡 /api/stream/stream called");
+  console.log("/api/stream/stream called");
 
   const token = req.query.token;
   if (!token) {
-    console.error("❌ No token provided");
+    console.error("No token provided");
     return res.status(401).send("Unauthorized");
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("✅ Token verified for user:", decoded.userId);
+    console.log("Token verified for user:", decoded.userId);
     req.user_id = decoded.userId;
   } catch (err) {
-    console.error("❌ JWT verify error:", err.message);
+    console.error("JWT verify error:", err.message);
     return res.status(401).send("Unauthorized");
   }
 
@@ -28,16 +28,15 @@ router.get("/stream", (req, res) => {
   res.flushHeaders();
 
   addClient(res);
-  console.log("📶 SSE connection established:", req.user_id);
+  console.log("SSE connection established:", req.user_id);
 
   const keepAlive = setInterval(() => res.write(":\n\n"), 20000);
 
   req.on("close", () => {
     clearInterval(keepAlive);
     removeClient(res);
-    console.log("❎ SSE connection closed:", req.user_id);
+    console.log("SSE connection closed:", req.user_id);
   });
 });
-
 
 module.exports = router;
